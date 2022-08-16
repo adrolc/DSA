0681fc63faff10e902235a15850b5b6d9ab4be38
Adrian Olczak <adrianolczakdev@gmail.com>
2022-08-16 19:56:43 +0200

    Create packages and add setup script

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='DSA',
+      version='1.0',
+      description='Data Structures & Algorithms',
+      author='Adrian Olczak',
+      author_email='adrianolczakdev@gmail.com',
+      packages=['algorithms', 'data-structures']
+)

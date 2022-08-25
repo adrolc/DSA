588edc024c68efaa4285f98d866c61e70e8a96e1
Adrian Olczak <adrianolczakdev@gmail.com>
2022-08-26 00:37:19 +0200

    Refactor: dynamically generated test suite for bubble sort

---
+++ tests/data/sorting.py
@@ -0,0 +1,12 @@
+
+sorting_data = {
+    "positive_numbers": [nums := [7, 4, 1, 2, 12, 10, 3, 5], sorted(nums)],
+    "negative_numbers": [nums := [-1, -3, -12, -5, -7, -2], sorted(nums)],
+    "positive_and_negative_numbers": [nums := [-5, 2, -14, 15, 4, 1, -3, 2], sorted(nums)],
+    "same_numbers": [nums := [5, 5, 5, 5, 5], sorted(nums)],
+    "reversed": [nums := [5, 4, 3, 2, 1], sorted(nums)],
+    "one_element": [nums := [1], sorted(nums)],
+    "empty_list": [nums := [], sorted(nums)],
+    "already_sorted": [nums := [1, 2, 3, 4, 5], sorted(nums)],
+    "repetitions": [nums := [5, 2, 6, 2, 5, 1, 2, 8, 6], sorted(nums)],
+}
--- tests/test_algorithms_sorting_bubble_sort.py
+++ tests/test_algorithms_sorting_bubble_sort.py
@@ -1,48 +1,27 @@
 import unittest
 
+from data.sorting import sorting_data
 from algorithms.sorting.bubble_sort.bubble_sort import bubble_sort
 
 
 class TestBubbleSort(unittest.TestCase):
-    def _test_sort(self, sorting_func, input):
-        expected_output = sorted(input)
-        self.assertEqual(sorting_func(input), expected_output)
+    pass
 
-    def test_bubble_sort_with_positive_numbers(self):
-        input = [7, 4, 1, 2, 12, 10, 3, 5]
-        self._test_sort(bubble_sort, input)
 
-    def test_bubble_sort_with_negative_numbers(self):
-        input = [-1, -3, -12, -5, -7, -2]
-        self._test_sort(bubble_sort, input)
+# Dynamically generated test methods.
+# Name and data are taken form the sorting_data dictionary as:
+# key: str = "name of dataset"
+# value: list[list[int]] = [list_to_sorted, sorted_list]
+def add_method(cls, name, data):
+    def method(self):
+        self.assertEqual(bubble_sort(data[0]), data[1])
 
-    def test_bubble_sort_with_positive_and_negative_numbers(self):
-        input = [-5, 2, -14, 15, 4, 1, -3, 2]
-        self._test_sort(bubble_sort, input)
+    method.__name__ = f"test_bubble_sort_{name}"
+    setattr(cls, method.__name__, method)
 
-    def test_bubble_sort_same_numbers(self):
-        input = [5, 5, 5, 5, 5]
-        self._test_sort(bubble_sort, input)
 
-    def test_bubble_sort_reversed(self):
-        input = [5, 4, 3, 2, 1]
-        self._test_sort(bubble_sort, input)
-
-    def test_bubble_sort_one_element(self):
-        input = [1]
-        self._test_sort(bubble_sort, input)
-
-    def test_bubble_sort_empty_list(self):
-        input = []
-        self._test_sort(bubble_sort, input)
-
-    def test_bubble_sort_already_sorted(self):
-        input = [1, 2, 3, 4, 5]
-        self._test_sort(bubble_sort, input)
-
-    def test_bubble_sort_with_repetitions(self):
-        input = [5, 2, 6, 2, 5, 1, 2, 8, 6]
-        self._test_sort(bubble_sort, input)
+for k, v in sorting_data.items():
+    add_method(TestBubbleSort, k, v)
 
 
 if __name__ == "__main__":
